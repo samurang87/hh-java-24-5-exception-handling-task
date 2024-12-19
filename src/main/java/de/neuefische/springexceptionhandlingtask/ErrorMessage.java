198406c524e6995e54c7d01b18faf09448521089
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2024-12-19 11:45:52 +0100

    feat: Add timestamp

--- src/main/java/de/neuefische/springexceptionhandlingtask/ErrorMessage.java
+++ src/main/java/de/neuefische/springexceptionhandlingtask/ErrorMessage.java
@@ -2,5 +2,10 @@ package de.neuefische.springexceptionhandlingtask;
 
 import org.springframework.http.HttpStatus;
 
-public record ErrorMessage(HttpStatus status, String message) {
+import java.time.LocalDateTime;
+
+public record ErrorMessage(HttpStatus status, String message, LocalDateTime timestamp) {
+    public ErrorMessage(HttpStatus status, String message) {
+        this(status, message, LocalDateTime.now());
+    }
 }
